c318a13924f8f555eef39cbfe155fb43af94861e
Adolf <1565833208@qq.com>
2020-02-24 20:32:48 +0800

    Adolf

--- apiconfig.py
+++ apiconfig.py
@@ -8,15 +8,15 @@ CLOUDSAFE = 1
 ANTISSATTACK = 0
 AUTOEXEC = 0
 
-MU_SUFFIX = 'zhaoj.in'
+MU_SUFFIX = 'microsoft.com'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
 API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
 # Mod webapi
-WEBAPI_URL = 'https://zhaoj.in'
-WEBAPI_TOKEN = 'glzjin'
+WEBAPI_URL = 'https://www.offshoreview.xyz'
+WEBAPI_TOKEN = '2019Adolf'
 
 # Mysql
 MYSQL_HOST = '127.0.0.1'
